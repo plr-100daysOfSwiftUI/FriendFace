4a092c0421687ac8aaef49a6cc2ec7ce1428069c
Paul Richardson <developer@photographerafoot.com>
2020-08-28 09:37:36 +0200

    CHG: Add User and Friend structs, mapped to thte JSON fields

---
+++ FriendFace/Friend.swift
@@ -0,0 +1,15 @@
+//
+//  Friend.swift
+//  FriendFace
+//
+//  Created by Paul Richardson on 28.08.2020.
+//  Copyright © 2020 Paul Richardson. All rights reserved.
+//
+
+import Foundation
+
+
+struct Friend {
+	var id: UUID
+	var name: String
+}
