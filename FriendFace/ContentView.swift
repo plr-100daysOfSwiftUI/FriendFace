4d3d7c4cc8cbc286b6473bfe00dcc1b83072f44f
Paul Richardson <developer@photographerafoot.com>
2020-08-28 13:27:46 +0200

    CHG: Load the data; fetch the json file from the server, decode it and load it into the users array

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -13,6 +13,24 @@ struct ContentView: View {
         Text("Hello, World!")
     }
 	@State private var users = [User]()
+	func loadData() {
+		let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
+		let request = URLRequest(url: url)
+		
+		URLSession.shared.dataTask(with: request) { data, response, error in
+			guard let data = data else {
+				return
+			}
+			
+			let decoder = JSONDecoder()
+			if let decodedData = try? decoder.decode([User].self, from: data) {
+				self.users.append(contentsOf: decodedData)
+			}
+			
+		}
+		.resume()
+
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
