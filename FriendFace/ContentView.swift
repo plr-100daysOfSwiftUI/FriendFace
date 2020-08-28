5c3d2ecfdd0c6edfc6b80144375ec77fcfee6a27
Paul Richardson <developer@photographerafoot.com>
2020-08-28 09:27:42 +0200

    Initial Commit

---
+++ FriendFace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FriendFace
+//
+//  Created by Paul Richardson on 28.08.2020.
+//  Copyright © 2020 Paul Richardson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
