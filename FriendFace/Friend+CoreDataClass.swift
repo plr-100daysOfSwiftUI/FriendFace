62d24da98750cc15bc2dcccb394bb3eab37259bc
Paul Richardson <developer@photographerafoot.com>
2020-08-29 17:53:14 +0200

    CHG: Subclass NSManagedObject for Friend

---
+++ FriendFace/Friend+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Friend+CoreDataClass.swift
+//  FriendFace
+//
+//  Created by Paul Richardson on 29.08.2020.
+//  Copyright © 2020 Paul Richardson. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Friend)
+public class Friend: NSManagedObject {
+
+}
---
+++ FriendFace/Friend+CoreDataProperties.swift
@@ -0,0 +1,41 @@
+//
+//  Friend+CoreDataProperties.swift
+//  FriendFace
+//
+//  Created by Paul Richardson on 29.08.2020.
+//  Copyright © 2020 Paul Richardson. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Friend {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Friend> {
+        return NSFetchRequest<Friend>(entityName: "Friend")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var name: String?
+    @NSManaged public var user: NSSet?
+
+}
+
+// MARK: Generated accessors for user
+extension Friend {
+
+    @objc(addUserObject:)
+    @NSManaged public func addToUser(_ value: User)
+
+    @objc(removeUserObject:)
+    @NSManaged public func removeFromUser(_ value: User)
+
+    @objc(addUser:)
+    @NSManaged public func addToUser(_ values: NSSet)
+
+    @objc(removeUser:)
+    @NSManaged public func removeFromUser(_ values: NSSet)
+
+}
