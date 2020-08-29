b7253036487ff80f2dd6ff03ddbc3325cbcfae08
Paul Richardson <developer@photographerafoot.com>
2020-08-29 18:07:34 +0200

    CHG: Add Decodable conformance to Friend

--- FriendFace/Friend+CoreDataClass.swift
+++ FriendFace/Friend+CoreDataClass.swift
@@ -11,6 +11,16 @@ import Foundation
 import CoreData
 
 @objc(Friend)
-public class Friend: NSManagedObject {
+public class Friend: NSManagedObject, Decodable {
+	
+	enum CodingKeys: CodingKey {
+		case id, name
+	}
 
+	required public convenience init(from decoder: Decoder) throws {
+		self.init()
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		id = try container.decode(UUID.self, forKey: .id)
+		name = try container.decode(String.self, forKey: .name)
+	}
 }
