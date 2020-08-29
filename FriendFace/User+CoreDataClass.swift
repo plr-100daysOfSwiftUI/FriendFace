a7ab3e1693b87e89815fc8ce2ddc353e497eda63
Paul Richardson <developer@photographerafoot.com>
2020-08-29 18:10:07 +0200

    CHG: Subclass NSManagedObject for User

---
+++ FriendFace/User+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  User+CoreDataClass.swift
+//  FriendFace
+//
+//  Created by Paul Richardson on 29.08.2020.
+//  Copyright © 2020 Paul Richardson. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
---
+++ FriendFace/User+CoreDataProperties.swift
@@ -0,0 +1,49 @@
+//
+//  User+CoreDataProperties.swift
+//  FriendFace
+//
+//  Created by Paul Richardson on 29.08.2020.
+//  Copyright © 2020 Paul Richardson. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension User {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<User> {
+        return NSFetchRequest<User>(entityName: "User")
+    }
+
+    @NSManaged public var about: String?
+    @NSManaged public var address: String?
+    @NSManaged public var age: Int16
+    @NSManaged public var company: String?
+    @NSManaged public var email: String?
+    @NSManaged public var id: UUID?
+    @NSManaged public var isActive: Bool
+    @NSManaged public var name: String?
+    @NSManaged public var registered: String?
+    @NSManaged public var tags: NSObject?
+    @NSManaged public var friends: NSSet?
+
+}
+
+// MARK: Generated accessors for friends
+extension User {
+
+    @objc(addFriendsObject:)
+    @NSManaged public func addToFriends(_ value: Friend)
+
+    @objc(removeFriendsObject:)
+    @NSManaged public func removeFromFriends(_ value: Friend)
+
+    @objc(addFriends:)
+    @NSManaged public func addToFriends(_ values: NSSet)
+
+    @objc(removeFriends:)
+    @NSManaged public func removeFromFriends(_ values: NSSet)
+
+}
