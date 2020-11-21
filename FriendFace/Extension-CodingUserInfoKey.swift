74f51f142cd5b73a67338ccd4e0bbc385eaf2e80
Paul Richardson <developer@photographerafoot.com>
2020-11-22 10:19:33 +1100

    CHG: Extend CodingUserInfoKey with a context key

---
+++ FriendFace/Extension-CodingUserInfoKey.swift
@@ -0,0 +1,13 @@
+//
+//  Extension-CodingUserInfoKey.swift
+//  FriendFace
+//
+//  Created by Paul Richardson on 22/11/2020.
+//  Copyright © 2020 Paul Richardson. All rights reserved.
+//
+
+import Foundation
+
+extension CodingUserInfoKey {
+	static let context = CodingUserInfoKey(rawValue: "context")
+}
