fd5c21e919e9ec880dc65d9ad4da54657ef62200
Paul Richardson <developer@photographerafoot.com>
2020-11-22 14:26:35 +1100

    CHG: Add Friend Detail View

---
+++ FriendDetailView.swift
@@ -0,0 +1,26 @@
+//
+//  FriendDetailView.swift
+//  FriendFace
+//
+//  Created by Paul Richardson on 22/11/2020.
+//  Copyright © 2020 Paul Richardson. All rights reserved.
+//
+
+import SwiftUI
+
+struct FriendDetailView: View {
+	
+	let friend: Friend
+	
+	var body: some View {
+		Text("Hello, \(friend.wrappedName)")
+	}
+}
+
+struct FriendDetailView_Previews: PreviewProvider {
+	static var previews: some View {
+//		TODO: Provide a Friend for preview
+//		FriendDetailView()
+		Text("Hallo, Friend!")
+	}
+}
